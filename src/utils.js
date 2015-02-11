4fc2b4073bafb8c62bbda73660bdb2c3a7d5cf90
AceMood <zmike86@163.com>
2015-02-11 18:58:24 +0800

    add ie11 getCurrentScript support and code style

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,11 @@
 
 // store useful props
 var OP = Object.prototype,
-    AP = Array.prototype,
-    native_forEach = AP.forEach,
-    native_map = AP.map,
-    hasOwn = OP.hasOwnProperty,
-    toString = OP.toString;
+  AP = Array.prototype,
+  native_forEach = AP.forEach,
+  native_map = AP.map,
+  hasOwn = OP.hasOwnProperty,
+  toString = OP.toString;
 
 
 // use such an object to determine cut down a forEach loop;
@@ -14,11 +14,11 @@ var break_obj = {};
 
 // initialize a module
 var empty_mod = {
-    id: null,
-    uid: null,
-    url: null,
-    status: null,
-    exports: {}
+  id: null,
+  uid: null,
+  url: null,
+  status: null,
+  exports: {}
 };
 
 
@@ -31,7 +31,7 @@ function noop() {}
  * will fail. we throw an error with useful message.
  */
 function exist_id_error(id) {
-    throw "more then one module defined with the same id: " + id;
+  throw "more then one module defined with the same id: " + id;
 }
 
 
@@ -44,15 +44,15 @@ function exist_id_error(id) {
  * @return {!Array}
  */
 function map(arr, fn, opt_context) {
-    var ret = [];
-    if (native_map && arr.map === native_map) {
-        ret = arr.map(fn, opt_context)
-    } else if (arr.length === +arr.length) {
-        for (var i = 0; i < arr.length; ++i) {
-            ret.push(fn.call(opt_context || null, arr[i], i, arr))
-        }
+  var ret = [];
+  if (native_map && arr.map === native_map) {
+    ret = arr.map(fn, opt_context)
+  } else if (arr.length === +arr.length) {
+    for (var i = 0; i < arr.length; ++i) {
+      ret.push(fn.call(opt_context || null, arr[i], i, arr))
     }
-    return ret;
+  }
+  return ret;
 }
 
 
@@ -67,14 +67,14 @@ function map(arr, fn, opt_context) {
  * @param {Object?} opt_context fn's context
  */
 function forEach(arr, fn, opt_context) {
-    if (native_forEach && arr.forEach === native_forEach) {
-        arr.forEach(fn, opt_context);
-    } else if (arr.length === +arr.length) {
-        for (var i = 0, length = arr.length; i < length; i++) {
-            if (fn.call(opt_context, arr[i], i, arr) === break_obj)
-                break;
-        }
+  if (native_forEach && arr.forEach === native_forEach) {
+    arr.forEach(fn, opt_context);
+  } else if (arr.length === +arr.length) {
+    for (var i = 0, length = arr.length; i < length; i++) {
+      if (fn.call(opt_context, arr[i], i, arr) === break_obj)
+        break;
     }
+  }
 }
 
 
@@ -85,21 +85,21 @@ function forEach(arr, fn, opt_context) {
  * @return {Number}
  */
 function indexOf(arr, tar) {
-    for (var i = 0; i < arr.length; ++i) {
-        if (arr[i] === tar)
-            return i;
-    }
-    return -1;
+  for (var i = 0; i < arr.length; ++i) {
+    if (arr[i] === tar)
+      return i;
+  }
+  return -1;
 }
 
 
 var type_map = {
-    "[object Object]": "object",
-    "[object Array]" : "array",
-    "[object Function]": "function",
-    "[object RegExp]": "regexp",
-    "[object String]": "string",
-    "[object Number]": "number"
+  "[object Object]": "object",
+  "[object Array]" : "array",
+  "[object Function]": "function",
+  "[object RegExp]": "regexp",
+  "[object String]": "string",
+  "[object Number]": "number"
 };
 
 
@@ -107,7 +107,7 @@ var type_map = {
  * detect the obj's type
  */
 function typeOf(obj) {
-    return type_map[toString.call(obj)]
+  return type_map[toString.call(obj)]
 }
 
 
@@ -117,19 +117,19 @@ function typeOf(obj) {
  * @return {Boolean}
  */
 function isNull(obj) {
-    return obj === void 0 || obj === null;
+  return obj === void 0 || obj === null;
 }
 
 
 var doc = document,
-    head = doc.head || doc.getElementsByTagName("head")[0],
+  head = doc.head || doc.getElementsByTagName("head")[0],
 // It's a classical bug in IE6 found in jQuery.
 // see more: 'http://dev.jquery.com/ticket/2709'
-    $base = doc.getElementsByTagName("base")[0];
+  $base = doc.getElementsByTagName("base")[0];
 
 
 if ($base) {
-    head = $base.parentNode;
+  head = $base.parentNode;
 }
 
 
@@ -144,10 +144,10 @@ var currentAddingScript,
 // + 'ie10-dynamic-script-element-fires-loaded-readystate-prematurely'
 // 'https://connect.microsoft.com/IE/feedback/details/648057/'
 // + 'script-onload-event-is-not-fired-immediately-after-script-execution'
-    useInteractive = ('readyState' in doc.createElement("script")),
+  useInteractive = ('readyState' in doc.createElement("script")),
 // loop all script nodes in doc, if one's readyState is 'interactive'
 // means it's now executing;
-    interactiveScript;
+  interactiveScript;
 
 
 /**
@@ -159,33 +159,33 @@ var currentAddingScript,
  *   maybe a top-level name, relative name or absolute name.
  */
 function fetch(url, name) {
-    var script = doc.createElement("script");
-    script.charset = "utf-8";
-    script.async = true;
-    // custom attribute to remember the original required name
-    // which written in dependant module.
-    script.kernel_name = name;
+  var script = doc.createElement("script");
+  script.charset = "utf-8";
+  script.async = true;
+  // custom attribute to remember the original required name
+  // which written in dependant module.
+  script.kernel_name = name;
 
-    // Event binding
-    script.onreadystatechange = script.onload = script.onerror = function () {
-        script.onreadystatschange = script.onload = script.onerror = null;
-        interactiveScript = null;
-        if (!script.readyState || /complete/.test(script.readyState)) {
-            head.removeChild(script);
-        }
-    };
-
-    // Older IEs will request the js file once src has been set,
-    // then readyState will be "loaded" if script complete loading,
-    // but change to "complete" after the code executed.
-    script.src = url;
-    currentAddingScript = script;
-    if ($base) {
-        head.insertBefore(script, $base);
-    } else {
-        head.appendChild(script);
+  // Event binding
+  script.onreadystatechange = script.onload = script.onerror = function () {
+    script.onreadystatschange = script.onload = script.onerror = null;
+    interactiveScript = null;
+    if (!script.readyState || /complete/.test(script.readyState)) {
+      head.removeChild(script);
     }
-    currentAddingScript = null;
+  };
+
+  // Older IEs will request the js file once src has been set,
+  // then readyState will be "loaded" if script complete loading,
+  // but change to "complete" after the code executed.
+  script.src = url;
+  currentAddingScript = script;
+  if ($base) {
+    head.insertBefore(script, $base);
+  } else {
+    head.appendChild(script);
+  }
+  currentAddingScript = null;
 }
 
 
@@ -194,7 +194,7 @@ function fetch(url, name) {
  * @return {NodeList}
  */
 function scripts() {
-    return doc.getElementsByTagName("script");
+  return doc.getElementsByTagName("script");
 }
 
 
@@ -203,56 +203,96 @@ function scripts() {
  * @return {*}
  */
 function getCurrentScript() {
-    // todo: In chrome and FF and Opera, use Error.prototype.stack
-    // It's important to note that this will not reference the <script> element
-    // if the code in the script is being called as a callback or event handler;
-    // it will only reference the element while it's initially being processed.
-    // Read more:
-    //   'https://developer.mozilla.org/en-US/docs/Web/API/document.currentScript'
-    return doc.currentScript || currentAddingScript || (function() {
-        var _scripts;
-        if (useInteractive) {
-            if (interactiveScript && interactiveScript.readyState == "interactive")
-                return interactiveScript;
+  // In chrome and FF and Opera, use Error.prototype.stack
+  // It's important to note that this will not reference the <script> element
+  // if the code in the script is being called as a callback or event handler;
+  // it will only reference the element while it's initially being processed.
+  // Read more:
+  //   'https://developer.mozilla.org/en-US/docs/Web/API/document.currentScript'
+  return doc.currentScript || currentAddingScript || (function() {
+    var _scripts;
+    if (useInteractive) {
+      if (interactiveScript && interactiveScript.readyState == "interactive")
+        return interactiveScript;
 
-            _scripts = scripts();
-            forEach(_scripts, function(script) {
-                if (script.readyState == "interactive") {
-                    interactiveScript = script;
-                    return break_obj;
-                }
-            });
-            return interactiveScript;
+      _scripts = scripts();
+      forEach(_scripts, function(script) {
+        if (script.readyState == "interactive") {
+          interactiveScript = script;
+          return break_obj;
         }
-        // todo in FF early version
-        return null;
-    })() || (function() {
-        var ret = null;
-        var stack;
-        try {
-            throw new Error();
-        } catch(e) {
-            stack = e.stack;
-        }
+      });
+      return interactiveScript;
+    }
+    // todo in FF early version
+    // return null;
+  })() || (function() {
+    var ret = null;
+    var stack;
+    try {
+      throw new Error();
+    } catch(e) {
+      stack = e.stack;
+    }
 
-        if (!stack) return ret;
+    if (!stack) return ret;
 
-        // chrome uses at, FF uses @
-        var e = stack.indexOf(" at ") != -1 ? " at " : "@";
-        while (stack.indexOf(e) !== -1)
-            stack = stack.substring(stack.indexOf(e) + e.length);
-        stack = stack.substring(0, stack.indexOf(".js") + 3);
+    /**
+     * chrome uses at, FF uses @
+     * Also consider IE 11.
+     * FireFox: e.g.
+     * getCurrentScript/<@file:///D:/Develop/SOI/lib/kernel.js:261:15
+     * getCurrentScript@file:///D:/Develop/SOI/lib/kernel.js:257:1
+     * getCurrentPath@file:///D:/Develop/SOI/lib/kernel.js:314:16
+     * require@file:///D:/Develop/SOI/lib/kernel.js:563:29
+     * require.async@file:///D:/Develop/SOI/lib/kernel.js:1178:5
+     * bind/<@file:///D:/Develop/SOI/demo/assets/js/app.js:25:9
+     * F@file:///D:/Develop/SOI/demo/lib/events/util.js:2:4216
+     * q@file:///D:/Develop/SOI/demo/lib/events/util.js:2:1034
+     * y/a<@file:///D:/Develop/SOI/demo/lib/events/util.js:2:2610
+     *
+     * chrome 39.0 e.g.
+     * at file:///D:/Develop/SOI/lib/kernel.js:261:15
+     * at getCurrentScript (file:///D:/Develop/SOI/lib/kernel.js:294:7)
+     * at getCurrentPath (file:///D:/Develop/SOI/lib/kernel.js:314:16)
+     * at require (file:///D:/Develop/SOI/lib/kernel.js:563:29)
+     * at Function.require.async (file:///D:/Develop/SOI/lib/kernel.js:1178:5)
+     * at HTMLButtonElement.<anonymous> (file:///D:/Develop/SOI/demo/assets/js/app.js:25:17)
+     * at F (file:///D:/Develop/SOI/demo/lib/events/util.js:2:4218)
+     * at q (file:///D:/Develop/SOI/demo/lib/events/util.js:2:1034)
+     * at HTMLButtonElement.<anonymous> (file:///D:/Develop/SOI/demo/lib/events/util.js:2:2610)"
+     *
+     * IE11 e.g.
+     * at Anonymous function (file:///D:/Develop/SOI/lib/kernel.js:294:7)
+     * at getCurrentPath (file:///D:/Develop/SOI/lib/kernel.js:314:16)
+     * at Global code (file:///D:/Develop/SOI/lib/kernel.js:563:29)
+     */
+    var e = stack.indexOf(" at ") != -1 ? " at " : "@";
+    var index = stack.indexOf(".async");
+    if (index > -1) {
+      stack = stack.substring(index + 7);
+      stack = stack.split(e)[1];
+      stack = stack.replace(/^([^\(]*\()/, '');
+    } else {
+      while (stack.indexOf(e) !== -1) {
+        stack = stack.substring(stack.indexOf(e) + e.length);
+      }
+    }
 
-        var _scripts = scripts();
-        forEach(_scripts, function(script) {
-            var path = getAbsPathOfScript(script);
-            if (path == stack) {
-                ret = script;
-                return break_obj;
-            }
-        });
-        return ret;
-    })();
+    stack = stack.substring(0, stack.indexOf(".js") + 3);
+    // for ie11
+    stack = stack.replace(/^([^\(]*\()/, "");
+
+    var _scripts = scripts();
+    forEach(_scripts, function(script) {
+      var path = getAbsPathOfScript(script);
+      if (path == stack) {
+        ret = script;
+        return break_obj;
+      }
+    });
+    return ret;
+  })();
 }
 
 
@@ -262,7 +302,7 @@ function getCurrentScript() {
  * @return {*}
  */
 function getAbsPathOfScript(script) {
-    return script.hasAttribute ? script.src : script.getAttribute("src", 4);
+  return script.hasAttribute ? script.src : script.getAttribute("src", 4);
 }
 
 
@@ -272,6 +312,6 @@ function getAbsPathOfScript(script) {
  * @return {*|String}
  */
 function getCurrentPath() {
-    var node = getCurrentScript();
-    return node && getAbsPathOfScript(node);
+  var node = getCurrentScript();
+  return node && getAbsPathOfScript(node);
 }
