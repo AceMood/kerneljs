c44318c121aeada40b4f620096c4b174b1c5ee84
AceMood <zmike86@163.com>
2015-02-11 18:43:14 +0800

    export short global name

--- src/kernel.js
+++ src/kernel.js
@@ -5,7 +5,7 @@ var kernel = {};
 
 // preserve existed kernel object;
 if (global.kernel) {
-    kernel._kernel = global.kernel;
+  kernel._kernel = global.kernel;
 }
 
 
@@ -17,22 +17,22 @@ kernel.uidprefix = "AceMood@kernel_";
 // All modules being fetched means the module's dependencies
 // is now fetching, and the key is mod's uid, value is mod itself;
 var fetchingList = {
-    mods: {},
-    add: function(mod) {
-        if (this.mods[mod.uid])
-            throw "current mod with uid: " + mod.uid +
-                " and file path: " + mod.url + " is fetching now";
-        this.mods[mod.uid] = mod;
-    },
-    clear: function() {
-        this.mods = {};
-    },
-    remove: function(mod) {
-        if (this.mods[mod.uid]) {
-            this.mods[mod.uid] = null;
-            delete this.mods[mod.uid];
-        }
+  mods: {},
+  add: function(mod) {
+    if (this.mods[mod.uid])
+      throw "current mod with uid: " + mod.uid +
+        " and file path: " + mod.url + " is fetching now";
+    this.mods[mod.uid] = mod;
+  },
+  clear: function() {
+    this.mods = {};
+  },
+  remove: function(mod) {
+    if (this.mods[mod.uid]) {
+      this.mods[mod.uid] = null;
+      delete this.mods[mod.uid];
     }
+  }
 };
 
 
@@ -55,55 +55,55 @@ var sendingList = {};
 /**
  * Dynamic config kernel.
  * property of obj can be:
- * [alias]: a collection of short names will be used to stand for 
+ * [alias]: a collection of short names will be used to stand for
  *     a long name or long path module.
  * [paths]: a hash
  * [baseUrl]:
  */
 kernel.config = function(obj) {
-    if (typeOf(obj) != "object")
-        throw "config object must an object";
-    var key, k;
-    for (key in obj) {
-        if (hasOwn.call(obj, key)) {
-            if (kernel[key]) {
-                for (k in obj[key]) {
-                    kernel[key][k] = obj[key][k];
-                }
-            } else
-                kernel[key] = obj[key];
+  if (typeOf(obj) != "object")
+    throw "config object must an object";
+  var key, k;
+  for (key in obj) {
+    if (hasOwn.call(obj, key)) {
+      if (kernel[key]) {
+        for (k in obj[key]) {
+          kernel[key][k] = obj[key][k];
         }
+      } else
+        kernel[key] = obj[key];
     }
+  }
 };
 
 
 // Global cache.
 kernel.cache = {
-    // use a global cache to store uid-module pairs.
-    // each uid mapping to a unique module, so it's a
-    // one-to-one hash constructor.
-    mods: {},
-    // and id2path record all module that have a user-defined id.
-    // its a pairs; not all modules have user-defined id, so this object
-    // if lack of some modules in debug mode;
-    // But imagine build, all modules will have self-generated id.
-    // It's a one-to-one hash constructor, because a user-defined id
-    // can only defined in one file.
-    id2path: {},
-    // each file may have multiple modules. so it's a one-to-many hash
-    // constructor.
-    path2uid: {}
+  // use a global cache to store uid-module pairs.
+  // each uid mapping to a unique module, so it's a
+  // one-to-one hash constructor.
+  mods: {},
+  // and id2path record all module that have a user-defined id.
+  // its a pairs; not all modules have user-defined id, so this object
+  // if lack of some modules in debug mode;
+  // But imagine build, all modules will have self-generated id.
+  // It's a one-to-one hash constructor, because a user-defined id
+  // can only defined in one file.
+  id2path: {},
+  // each file may have multiple modules. so it's a one-to-many hash
+  // constructor.
+  path2uid: {}
 };
 
 
 // default built-in modules
 // map the short name and relative path?
 kernel.config({
-    baseUrl: "",
-    debug: true,
-    builtin: {
+  baseUrl: "",
+  debug: true,
+  builtin: {
 
-    }
+  }
 });
 
 
@@ -111,15 +111,15 @@ kernel.config({
  * Clear all cache.
  */
 kernel.reset = function() {
-    kernel.cache = {
-        mods: {},
-        id2path: {},
-        path2uid: {}
-    };
+  kernel.cache = {
+    mods: {},
+    id2path: {},
+    path2uid: {}
+  };
 };
 
 
 // exports APIs functions
-global.require = require;
-global.define = define;
+global.require = global._req = require;
+global.define = global._def = define;
 global.kernel = kernel;
