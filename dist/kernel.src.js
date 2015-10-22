cc32212280fb3ac7e2e85edcc6a64f3f1a256784
AceMood <zmike86@163.com>
2015-10-23 07:33:50 +0800

    dev

--- dist/kernel.src.js
+++ dist/kernel.src.js
@@ -1368,7 +1368,7 @@ kerneljs.url = function(url) {
 
 kerneljs.on = on;
 kerneljs.emit = emit;
-kerneljs.request = fetchScript;
+//kerneljs.request = fetchScript;
 kerneljs.eventsType = events;
 kerneljs.data = {};
 
